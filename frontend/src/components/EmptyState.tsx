407bf666a7e6505d37b2bb2b08ce37c5dca6c958
Steve Dukes <stdukes@gmail.com>
2026-01-11 13:44:55 -0500

    refactor(EmptyState): Created component for empty state UI
    This component handles when no file has been uploaded

---
+++ frontend/src/components/EmptyState.tsx
@@ -0,0 +1,9 @@
+import './EmptyState.css';
+
+export const EmptyState = () => {
+  return (
+    <div className="empty-state">
+      Ask a question about your document to get started
+    </div>
+  );
+};
